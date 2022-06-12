5a04b15261699ed6da6cc9032ed18d4d7092254d
b-hexsoul <brandon.hexsel@gmail.com>
2022-06-12 11:42:15 -0700

    init project

---
+++ backend/src/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const colors = require("colors");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const connectDB = require("../config/db");
+const schema = require("./graphql/schemas/schema");
+const port = process.env.PORT || 5000;
+
+const main = () => {
+  const app = express();
+  // connect to mongoDB
+  connectDB();
+  app.use(
+    "/graphql",
+    graphqlHTTP({
+      schema,
+      graphiql: process.env.NODE_ENV === "development",
+    })
+  );
+
+  app.use("/", (req, res) => {
+    res.send("<h1>Hello World</h1>");
+  });
+
+  app.listen(port, console.log(`Listening on port ${port}`));
+};
+
+main();
