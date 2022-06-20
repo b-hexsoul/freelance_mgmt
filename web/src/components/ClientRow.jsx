6d5edd35b75189c2dc93354dcd9d690f3b89503e
b-hexsoul <brandon.hexsel@gmail.com>
2022-06-19 22:23:13 -0700

    Update client delete to cascade to projects

--- web/src/components/ClientRow.jsx
+++ web/src/components/ClientRow.jsx
@@ -2,6 +2,7 @@ import { useMutation } from '@apollo/client';
 import { FaTrash } from 'react-icons/fa';
 import { DELETE_CLIENT } from '../mutations/clientMutations';
 import { GET_CLIENTS } from '../queries/clientQueries';
+import { GET_PROJECTS } from '../queries/projectQueries';
 
 export default function ClientRow({ client }) {
   const { id, name, email, phone } = client;
@@ -10,9 +11,14 @@ export default function ClientRow({ client }) {
     // refetchQueries: [{ query: GET_CLIENTS }]
     update(cache, { data: { deleteClient } }) {
       const { clients } = cache.readQuery({ query: GET_CLIENTS });
+      const { projects } = cache.readQuery({ query: GET_PROJECTS });
       cache.writeQuery({
         query: GET_CLIENTS,
         data: { clients: clients.filter(client => client.id !== deleteClient.id) },
+      })
+      cache.writeQuery({
+        query: GET_PROJECTS,
+        data: { projects: projects.filter(project => project.clientId !== deleteClient.id)}
       })
     }
   });
